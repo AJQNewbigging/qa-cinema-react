5721c46e2bb737e3f00c5f85e2eebe0aa6f223ee
Jools Arts <jools.arts@gmail.com>
2022-05-24 14:56:39 +0100

    Added the JavaScript functionality to the SearchBar file, whilst also modifying the file extension

--- qa-cinema/src/pages/Search.js
+++ qa-cinema/src/pages/Search.js
@@ -1,18 +1,8 @@
 import React from 'react'
+import SearchBar from '../components/SearchBar';
 
 function Search() {
     return (
-        <>
-            <div class="search-wrapper">
-                <label for="search">Search Movies</label>
-                <input type="search" id="search" data-search></input>
-            </div>
-            <div class="movie-cards" data-movie-cards-container></div>
-            <template data-movie-template></template>
-                <div class="card">
-                    <div class="header" data-header></div>
-                    <div class="body" data-body></div>
-            </div>
-        </>
+        <SearchBar />
     );
 };
