722cc268913f06d37b9ecc0fa695047bc318fe11
Jools Arts <jools.arts@gmail.com>
2022-05-25 14:39:00 +0100

    Adjusted the SearchBar function within SearchBar.jsx to use an onChange event handler, rather than an event listener

--- qa-cinema/src/components/SearchBar.jsx
+++ qa-cinema/src/components/SearchBar.jsx
@@ -5,17 +5,16 @@ function SearchBar() {
 
     const movieCardTemplate = document.querySelector("[data-movie-template]")
     const movieCardContainer = document.querySelector("[data-movie-cards-container]")
-    const searchInput = document.querySelector("[data-search]")
     
     let movies = []
     
-    searchInput.addEventListener("input", i => {
+    function handleChange(i) {
         const value = i.target.value.toLowerCase()
         movies.forEach(movie => {
             const isVisible = movie.name.toLowerCase().includes(value) || movie.genre.toLowerCase().includes(value)
             movie.element.classList.toggle("hide", !isVisible)
         })
-    })
+    }
     
     fetch("http://127.0.0.1:3000/movie")
         .then(res => res.json())
@@ -35,7 +34,7 @@ function SearchBar() {
      <>
         <div class="search-wrapper">
             <label for="search">Search All Listings</label>
-            <input type="search" id="search" placeholder="Search..." data-search></input>
+            <input type="search" id="search" placeholder="Search..." onChange={handleChange}></input>
         </div>
         <div class="movie-cards" data-movie-cards-container></div>
         <template data-movie-template>
@@ -47,5 +46,54 @@ function SearchBar() {
     </>
   )
 }
+
+// Original solution using an event listener
+
+// function SearchBar() {
+
+//     const movieCardTemplate = document.querySelector("[data-movie-template]")
+//     const movieCardContainer = document.querySelector("[data-movie-cards-container]")
+//     const searchInput = document.querySelector("[data-search]")
+    
+//     let movies = []
+    
+//     searchInput.addEventListener("input", i => {
+//         const value = i.target.value.toLowerCase()
+//         movies.forEach(movie => {
+//             const isVisible = movie.name.toLowerCase().includes(value) || movie.genre.toLowerCase().includes(value)
+//             movie.element.classList.toggle("hide", !isVisible)
+//         })
+//     })
+    
+//     fetch("http://127.0.0.1:3000/movie")
+//         .then(res => res.json())
+//         .then(data => {
+//         movies = data.map(movie => {
+//             const card = movieCardTemplate.content.cloneNode(true).children[0]
+//             const header = card.querySelector("[data-header]")
+//             const body = card.querySelector("[data-body]")
+//             header.textContent = movie.name
+//             body.textContent = movie.genre
+//             movieCardContainer.append(card)
+//             return {name: movie.name, genre: movie.genre, element: card}
+//         })
+//     })
+    
+//   return (
+//      <>
+//         <div class="search-wrapper">
+//             <label for="search">Search All Listings</label>
+//             <input type="search" id="search" placeholder="Search..." onChange={handleChange} data-search></input>
+//         </div>
+//         <div class="movie-cards" data-movie-cards-container></div>
+//         <template data-movie-template>
+//             <div class="card">
+//                 <div class="header" data-header></div>
+//                 <div class="body" data-body></div>
+//             </div>
+//         </template>
+//     </>
+//   )
+// }
 
 export default SearchBar
